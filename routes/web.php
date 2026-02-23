4e83b9737a1d4970a3f7ad620e2bcbaaed361f60
Indhirayc <indhirayuantikac1176@gmail.com>
2026-02-23 16:14:42 +0700

    Jobsheet02_Praktikum02

--- routes/web.php
+++ routes/web.php
@@ -34,3 +34,15 @@ use App\Http\Controllers\ArticleController;
 Route::get('/', HomeController::class);
 Route::get('/about', AboutController::class);
 Route::get('/articles/{id}', ArticleController::class);
+
+use App\Http\Controllers\PhotoController;
+
+// Route::resource('photos', PhotoController::class);
+
+// Route::resource('photos', PhotoController::class)->only([
+//     'index', 'show'
+// ]);
+
+Route::resource('photos', PhotoController::class)->except([
+    'create', 'store', 'update', 'destroy'
+]);
